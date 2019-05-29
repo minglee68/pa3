376f47dbba5d7842e8e31fddd56c45127bf17ca9
Min Young Lee <avadaisuki68@gmail.com>
2019-05-29 12:28:22 +0900

    updated ddetector.c

--- ddetector.c
+++ ddetector.c
@@ -7,22 +7,13 @@
 #include <time.h>
 #include <unistd.h>
 
-#define N 200
-
-static pthread_mutex_t *graph[10][100];
+static pthread_mutex_t threadmutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int lock[10][100];
 static int try[100][10];
 
 static pthread_mutex_t *mutex_index[100] = {NULL};
 static unsigned int thread_index[10] = {0};
-
-
-pthread_mutex_t lock_mutex1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_mutex2 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t lock_mutex3 = PTHREAD_MUTEX_INITIALIZER;
-
-static int n = 0;
 
 int check_lock(int orig_thread, int mutex_flag);
 int check_try(int orig_thread, int thread_flag);
@@ -33,8 +24,7 @@ int check_lock(int orig_thread, int mutex_flag) {
 	int i = 0;
 	for(i = 0; i < 10; i++) {
 		if (lock[i][mutex_flag] == 1) {
-			//fprintf(stderr, "there is lock on %p by %d\n", mutex_index[mutex_flag], thread_index[i]);
-			fprintf(stderr, "->lock on m%d by t%d", mutex_flag, i);
+			fprintf(stderr, "->lock on m%d by t%d(%d)", mutex_flag, i, orig_thread);
 			if (i == orig_thread)
 				return -1;
 			return check_try(orig_thread, i);
@@ -48,8 +38,7 @@ int check_try(int orig_thread, int thread_flag) {
 	int i = 0;
 	for (i = 0; i < 100; i++) {
 		if (try[i][thread_flag] == 1) {
-			//fprintf(stderr, "there is trial on %p by %d\n", mutex_index[i], thread_index[thread_flag]);
-			fprintf(stderr, "->try by t%d on m%d", thread_flag, i);
+			fprintf(stderr, "->try by t%d on m%d(%d)", thread_flag, i, orig_thread);
 			return check_lock(orig_thread, i);
 		}
 	}
@@ -57,11 +46,11 @@ int check_try(int orig_thread, int thread_flag) {
 	return 0;
 }
 
+/*
 int check_lock_a(int orig_mutex, int mutex_flag) {
 	int i = 0;
 	for(i = 0; i < 10; i++) {
 		if (lock[i][mutex_flag] == 1) {
-			//fprintf(stderr, "there is lock on %p by %d\n", mutex_index[mutex_flag], thread_index[i]);
 			fprintf(stderr, "->lock on m%d by t%d", mutex_flag, i);
 			return check_try_a(orig_mutex, i);
 		}
@@ -74,7 +63,6 @@ int check_try_a(int orig_mutex, int thread_flag) {
 	int i = 0;
 	for (i = 0; i < 100; i++) {
 		if (try[i][thread_flag] == 1) {
-			//fprintf(stderr, "there is trial on %p by %d\n", mutex_index[i], thread_index[thread_flag]);
 			fprintf(stderr, "->try by t%d on m%d", thread_flag, i);
 			if (i == orig_mutex)
 				return -1;
@@ -84,6 +72,7 @@ int check_try_a(int orig_mutex, int thread_flag) {
 	
 	return 0;
 }
+*/
 
 int pthread_mutex_lock(pthread_mutex_t *mutex) {
 
@@ -98,28 +87,6 @@ int pthread_mutex_lock(pthread_mutex_t *mutex) {
 	pthread_mutex_unlockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 	if ((error = dlerror()) != 0x0)
 		exit(1);
-
-	//fprintf(stderr, "lock trial by %d!!\n", (unsigned int)pthread_self());
-
-/*
-	int i = 0;
-	int j = 0;
-	int flag = 0;
-	for (i = 0; i < 10; i++) {
-		if (thread_index[i] == 0)
-			break;
-		for (j = 0; j < 100; j++) {
-			if (graph[i][j] == NULL)
-				break;
-			if (graph[i][j] == mutex) {
-				fprintf(stderr, "deadlock detected, %p, %p\n", graph[i][j], mutex);
-				flag = 1;
-			}
-		}
-	}
-*/
-	//fprintf(stderr, "lock called for %p by %d\n", mutex, (unsigned int)pthread_self());
-	//pthread_mutex_lockp(&lock_mutex1);
 
 	int i = 0;
 	int j = 0;
@@ -154,33 +121,28 @@ int pthread_mutex_lock(pthread_mutex_t *mutex) {
 		}
 	}
 
+	pthread_mutex_lockp(&threadmutex);
+
 	try[mutex_flag][thread_flag] = 1;
-	//fprintf(stderr, "trial to lock %p from %d\n", mutex_index[mutex_flag], thread_index[thread_flag]);
 
-	fprintf(stderr, "try by t%d on m%d", thread_flag, mutex_flag);
+	fprintf(stderr, "(start %d)try by t%d on m%d", thread_flag, thread_flag, mutex_flag);
 	if (check_lock(thread_flag, mutex_flag) == -1)
-		fprintf(stderr, "\nDeadlock Detected!\n");
-	fprintf(stderr, "\n");
+		fprintf(stderr, "\nDeadlock Detected!(%d)", thread_flag);
+	fprintf(stderr, "(end %d)\n", thread_flag);
 
-	//pthread_mutex_unlockp(&lock_mutex1);
+	pthread_mutex_unlockp(&threadmutex);
 
-	//pthread_mutex_lockp(&lock_mutex3);
 	int temp = pthread_mutex_lockp(mutex);
 
 	lock[thread_flag][mutex_flag] = 1;
 	try[mutex_flag][thread_flag] = 0;
 
-	fprintf(stderr, "lock on m%d by t%d", mutex_flag, thread_flag);
-	if (check_try_a(mutex_flag, thread_flag) == -1)
-		fprintf(stderr, "\nDeadlock Detected!\n");
-	fprintf(stderr, "\n");
+	fprintf(stderr, "lock on m%d by t%d\n", mutex_flag, thread_flag);
+	//fprintf(stderr, "lock on m%d by t%d", mutex_flag, thread_flag);
+	//if (check_try_a(mutex_flag, thread_flag) == -1)
+	//	fprintf(stderr, "\nDeadlock Detected!\n");
+	//fprintf(stderr, "\n");
 
-	//fprintf(stderr, "success to lock %p from %d\n", mutex_index[mutex_flag], thread_index[thread_flag]);
-
-	//fprintf(stderr, "%d locks %p\n", (unsigned int)pthread_self(), mutex);
-
-	//pthread_mutex_unlockp(&lock_mutex3);
-	
 	return temp;
 }
 
@@ -197,10 +159,6 @@ int pthread_mutex_unlock(pthread_mutex_t *mutex) {
 	pthread_mutex_unlockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 	if ((error = dlerror()) != 0x0)
 		exit(1);
-
-	//fprintf(stderr, "unlock called for %p by %d\n", mutex, (unsigned int)pthread_self());
-
-	//pthread_mutex_lockp(&lock_mutex2);
 
 	int i = 0;
 	int j = 0;
@@ -225,11 +183,6 @@ int pthread_mutex_unlock(pthread_mutex_t *mutex) {
 	lock[thread_flag][mutex_flag] = 0;
 	
 	fprintf(stderr, "unlock on m%d by t%d\n", mutex_flag, thread_flag);
-	//fprintf(stderr, "unlocked %p from %d\n", mutex_index[mutex_flag], thread_index[thread_flag]);
-
-	//fprintf(stderr, "%d unlocks %p\n", (unsigned int)pthread_self(), mutex);
-	
-	//pthread_mutex_unlockp(&lock_mutex2);
 
 	return temp;
 }
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {1..1000}
+do
+	LD_PRELOAD=./ddetector.so ./dinning_deadlock 2>> error.txt
+	echo ""
+done
